8aa44c058572c54e1c99d4f5c88e67d24aced0c2
YousefAmerAwad <1901524@eng.asu.edu.eg>
2022-09-16 10:07:02 +0200

    Finish

--- main.py
+++ main.py
@@ -10,10 +10,19 @@ inserts.wind.onkeypress(move.racket1_down,'s')      #move racket1 down while pre
 inserts.wind.onkeypress(move.racket2_up,'Up')       #move racket2 up while pressing 'Up'
 inserts.wind.onkeypress(move.racket2_down,'Down')   #move racket1 down while pressing 'Down'
 
+score1 = 0
+score2 = 0
+score = turtle.Turtle()
+score.color('white')
+score.speed(0)
+score.penup()
+score.hideturtle()
+score.goto(0,220)
+score.write('Player1: 0        Player2: 0',align='center',font=('courier',20,'normal'))
 
 
-dx = 0.1       #the change in x-axis
-dy = 0.05
+dx = 0.07       #the change in x-axis
+dy = 0.04
 #Main Game Loop
 while True:
     inserts.wind.update()   #update the screen
@@ -21,25 +30,33 @@ while True:
     inserts.ball.setx(inserts.ball.xcor() + dx)
     inserts.ball.sety(inserts.ball.ycor() + dy)
 
-    #if the ball hit the top border 
-    if inserts.ball.ycor() > 240:
-        inserts.ball.sety(240)
-        dy *= -1
 
-    if inserts.ball.ycor() < -240:
-        inserts.ball.sety(-240)
-        dy *= -1
 
-    if inserts.ball.xcor() > 300:
-        inserts.ball.goto(0,0)
-        dx *= -1
+    if inserts.ball.ycor() > 240:   #if the ball hits the upper border
+        inserts.ball.sety(240)      #let it bounce
+        dy *= -1     
 
-    if inserts.ball.xcor() < -300:
-        inserts.ball.goto(0,0)
-        dx *= -1
+    
+    if inserts.ball.ycor() < -240:   #if the ball hits the bottom
+        inserts.ball.sety(-240)      #let it will bounce 
+        dy *= -1       
 
-    if inserts.racket1.ycor() > 210:
-        inserts.racket1.sety(210)
+    if inserts.ball.xcor() > 300:    #if the ball hits the right border
+        inserts.ball.goto(0,0)       #return it back to the center
+        score1+=1
+        score.clear()
+        score.write('Player1: {}        Player2: {} '.format(score1,score2),align='center',font=('courier',20,'normal'))
+        dx *= -1                     #reverse it's direction
+
+    if inserts.ball.xcor() < -300:   #if the ball hits the left border  
+        inserts.ball.goto(0,0)       #return it back to the center
+        score2+=1
+        score.clear()
+        score.write('Player1: {}        Player2: {} '.format(score1,score2),align='center',font=('courier',20,'normal'))
+        dx *= -1                     #reverse it's direction
+
+    if inserts.racket1.ycor() > 210: #if the racket1 hits the upper border
+        inserts.racket1.sety(210)    #don't let it cross the border
 
     if inserts.racket2.ycor() > 210:
         inserts.racket2.sety(210)
@@ -49,6 +66,15 @@ while True:
 
     if inserts.racket1.ycor() < -210:
         inserts.racket1.sety(-210)
+
+    if (inserts.ball.xcor() > 250 and inserts.ball.xcor() < 260) and (inserts.ball.ycor() < inserts.racket2.ycor() + 50 and inserts.ball.ycor() > inserts.racket2.ycor() - 50):
+        inserts.ball.setx(240)
+        dx *= -1
+
+    if (inserts.ball.xcor() < -250 and inserts.ball.xcor() > -260) and (inserts.ball.ycor() < inserts.racket1.ycor() + 50 and inserts.ball.ycor() > inserts.racket1.ycor() - 50):
+        inserts.ball.setx(-240)
+        dx *= -1
+
 
 
 
